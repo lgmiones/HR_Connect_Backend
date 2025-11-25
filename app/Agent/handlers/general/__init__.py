cd85870a6a59a3fdbeba6efc79a7ea3b6d8796b9
sdalipio-svg <sdalipio@n-pax.com>
2025-11-25 15:44:15 +0800

    cleaned general handler

--- app/Agent/handlers/general/__init__.py
+++ app/Agent/handlers/general/__init__.py
@@ -1,22 +1,9 @@
 """General handler services exports"""
 
-from app.Agent.handlers.general.intent_detector import detect_general_intent
 from app.Agent.handlers.general.llm_responder import generate_llm_response, should_use_llm
-from app.Agent.handlers.general.templates import (
-    get_help_template,
-    get_about_template,
-    get_features_template,
-    get_greeting_template,
-    get_default_template
-)
+
 
 __all__ = [
-    "detect_general_intent",
     "generate_llm_response",
-    "should_use_llm",
-    "get_help_template",
-    "get_about_template",
-    "get_features_template",
-    "get_greeting_template",
-    "get_default_template"
+    "should_use_llm"
 ]
